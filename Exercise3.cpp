f9c70bc1f886183e4d0805657f8b2f2a1ada917a
tchenavier <thibaud.chenavier@gmail.com>
2026-01-08 15:36:14 +0100

    essay exo 4

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <string.h>
 
 
@@ -28,4 +28,4 @@ int main(int argc, char** argv)
 
 
 	return 0;
-}
+}*/
--- Exercise4.cpp
+++ Exercise4.cpp
@@ -1,29 +1,58 @@
-/*#include <stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-void saisie(float* var1, float* var2, float* var3)
+void saisie(float* aa, float* bb, float* cc)
 {
 	scanf_s("%f,var1");
 	scanf_s("%f,var2");
 	scanf_s("%f,var3");
 }
 
-float calcul(float a, float b, float c)
+float* calcul(float aa, float bb, float cc)
 {
-	float delta = 0;
-
-	if (delta > 0)
-	{
-		resulta = new float;
+	float delta = bb*bb - 4 * aa * cc;
+	
+	return &delta;
 	}
-}
 
 int main(int argc, char** argv)
 {
-    int var1, var2, var3;
-
-	saisie(&var1, &var2, &var3);
+   float a, b, c, *delta;
 
 
-	delete resulta ;
+	saisie(&a, &b, &c);
+	delta = calcul(a, b, c);
+
+	float* resulta = new float;
+	float* resulta2 = new float;
+
+	if (*delta > 0)
+	{
+		*resulta = -b + sqrt(*delta) / 2 * a;
+		*resulta2 = -b - sqrt(*delta) / 2 * a;
+
+		printf_s("%f\n", *delta);
+		printf_s("Il y a deux de solution Réelle.\n");
+		printf_s("%f\n", *resulta);
+		printf_s("%f\n", *resulta2);
+
+	}
+	else if (*delta == 0)
+	{
+		*resulta - b / 2 * a;
+		printf_s("%f\n", *delta);
+		printf_s("Il n'y a qu'une solution Réelle.\n");
+		printf_s("%f\n", *resulta);
+	}
+	else
+	{
+		printf_s("%f\n", *delta);
+		printf_s("Il n'y a pas de solution Réelle.\n");
+	}
+	
+
+	delete resulta;
+	delete resulta2;
+
     return 0;
-}*/
+}
